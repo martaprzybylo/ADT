6e31f250723389d1e262e49801b377da1092d353
Marta Przybyło <marta.m.przybylo@gmail.com>
2019-09-01 10:43:42 +0200

    slection sort for IList

--- src/main/java/pl/sda/List/MainList.java
+++ src/main/java/pl/sda/List/MainList.java
@@ -3,10 +3,17 @@ package pl.sda.List;
 public class MainList {
 
     public static void main(String[] args) {
-        IList list = new LinkedList();
+        SelectionSort ss = new SelectionSort();
+
+        IList list = new LinkedList<Integer>();
+        list.add(5);
         list.add(1);
         list.add(2);
-        list.add(3);
+        list.add(8);
+        list.add(0);
+        list.add(9);
+
+        list = ss.sort(list);
 
         Object[] tab = list.getHolderView();
 
@@ -16,7 +23,7 @@ public class MainList {
         System.out.println();
 
         int[] table = {16,4,9,8,12,2,5,0,6,3};
-        SelectionSort ss = new SelectionSort();
+
         int []sorted = ss.sort(table);
         for (int sor:sorted){
             System.out.print(sor + " ");
--- src/main/java/pl/sda/List/SelectionSort.java
+++ src/main/java/pl/sda/List/SelectionSort.java
@@ -2,6 +2,30 @@ package pl.sda.List;
 
 public class SelectionSort {
 
+    public IList sort(IList<Integer> listToSort){
+        int firstUsortedIndex = 0;
+
+        do {
+            int minIndex = findMinValueIndex(listToSort, firstUsortedIndex);
+            int tmp = listToSort.get(minIndex);
+            listToSort.set(minIndex, listToSort.get(firstUsortedIndex));
+            listToSort.set(firstUsortedIndex, tmp);
+        }while (++firstUsortedIndex!=listToSort.size());
+        return listToSort;
+    }
+
+    private int findMinValueIndex(IList<Integer> list, int startIndex) {
+        int minValue = list.get(startIndex);
+        int minValueIndex = startIndex;
+
+        for (int i = startIndex+1; i<list.size(); i++){
+            if (list.get(i)<minValue){
+                minValue = list.get(i);
+                minValueIndex = i;
+            }
+        } return minValueIndex;
+    }
+
     public int[] sort(int[] tableToSort) {
         int firstUsortedIndex = 0;
 
