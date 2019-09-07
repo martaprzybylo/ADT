2bd36187418a973918af41bd79644bc9f66c0804
Marta Przybyło <marta.m.przybylo@gmail.com>
2019-09-07 09:17:12 +0200

    quick sort i tree

--- src/main/java/pl/sda/List/MainList.java
+++ src/main/java/pl/sda/List/MainList.java
@@ -2,45 +2,55 @@ package pl.sda.List;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainList {
+    private static int upperBound = 10;
+    private static long startTime;
+    private static long startTime2;
+    private static long stoptTime;
+    private static long stoptTime2;
 
     public static void main(String[] args) {
         SelectionSort ss = new SelectionSort();
         QuickSort qs = new QuickSort();
+        Random random = new Random();
 
         IList list = new LinkedList<Integer>();
-        list.add(57);
-        list.add(1);
-        list.add(2);
-        list.add(8);
-        list.add(0);
-        list.add(4);
-        list.add(12);
-        list.add(9);
-        list.add(5);
+        for (int i = 0; i<=upperBound;i++){
+            list.add(random.nextInt(upperBound));
+        }
 
+        startTime = System.nanoTime();
         System.out.println("Selection Sort Linked Listy");
         list = ss.selSort(list);
-        System.out.println();
-        System.out.println("Quick Sort Linked Listy");
-        list = qs.sort(list, 0, list.size()-1);
-
-
         Object[] tab = list.getHolderView();
-
         for (Object val:tab)
             System.out.print(val + " ");
+        stoptTime = System.nanoTime();
+        long runnungTime = stoptTime-startTime;
+        System.out.println();
+
+        startTime2 = System.nanoTime();
+        System.out.println("Quick Sort Linked Listy");
+        list = qs.sort(list, 0, list.size()-1);
+        Object[] tab2 = list.getHolderView();
+        for (Object val:tab2)
+            System.out.print(val + " ");
+        stoptTime2 = System.nanoTime();
+        long runnungTime2 = stoptTime2-startTime2;
+        System.out.println();
 
         System.out.println();
         System.out.println("Selection Sort Tablicy");
-
         int[] table = {16,4,9,8,12,2,5,0,6,3};
-
         int []sorted = ss.sort(table);
         for (int sor:sorted){
             System.out.print(sor + " ");
         }
+        System.out.println();
+        System.out.println("SS potrzebuje "+ runnungTime + " nanosekund");
+        System.out.println("QS potrzebuje "+runnungTime2 + " nanosekund");
 
     }
 }
--- src/main/java/pl/sda/List/QuickSort.java
+++ src/main/java/pl/sda/List/QuickSort.java
@@ -1,23 +1,25 @@
 package pl.sda.List;
+import java.util.List;
 
 public class QuickSort {
-    public IList sort (IList<Integer>list, int startIndex, int stopIndex){
-        if (startIndex >= stopIndex)
-            return list;
+    public IList sort(IList<Integer> list2, int startIndex, int stopIndex) {
+        if(startIndex >= stopIndex)
+            return list2;
         int pivot = stopIndex;
-        pivot=splitTable(pivot,list,startIndex,stopIndex);
-        sort(list,startIndex,pivot-1);
-        return sort(list,pivot+1,stopIndex);
+        pivot = splitTable(pivot, list2, startIndex, stopIndex);
+        sort(list2, startIndex, pivot-1);
+        return sort(list2, pivot+1, stopIndex);
     }
 
-    private int splitTable(int pivot, IList<Integer> list, int startIndex, int stopIndex) {
-        for (int i = startIndex; i<pivot; i++){
-            if (list.get(i)>=list.get(pivot)){
-                list.add(pivot+1, list.get(i));
-                list.remove(i);
+    private int splitTable(int pivot, IList<Integer> list2, int startIndex, int stopIndex) {
+        for(int i = startIndex; i < pivot; i++) {
+            if(list2.get(i) >= list2.get(pivot)) {
+                list2.add(pivot+1, list2.get(i));
+                list2.remove(i);
                 pivot--;
                 i--;
             }
-        }return pivot;
+        }
+        return pivot;
     }
 }
